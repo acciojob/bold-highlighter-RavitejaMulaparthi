b5b8603a63fb460e5ed946170713592510e1a83b
RavitejaMulaparthi <137281804+RavitejaMulaparthi@users.noreply.github.com>
2023-10-06 15:11:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
+// Get the link element by its id
+const highlightLink = document.getElementById('highlightLink');
+
+// Function to highlight bold words
 function highlight() {
-    //Write your code here
+    const boldElements = document.querySelectorAll('strong');
 
-
+    // Change the color of all bold elements to green
+    for (const boldElement of boldElements) {
+        boldElement.style.color = 'green';
+    }
 }
 
-
+// Function to return the color to normal (black)
 function return_normal() {
-    //Write your code here
+    const boldElements = document.querySelectorAll('strong');
 
-    
+    // Revert the color of all bold elements back to black
+    for (const boldElement of boldElements) {
+        boldElement.style.color = 'black';
+    }
 }
+
+// Add event listeners for mouseover and mouseout events on the link
+highlightLink.addEventListener('mouseover', highlight);
+highlightLink.addEventListener('mouseout', return_normal);
